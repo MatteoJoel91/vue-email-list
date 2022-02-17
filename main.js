c9090252af34508a8ee85043796e15c83347d55d
Matteo Gioele Cocomazzi <TheZefyro@gmail.com>
2022-02-17 16:14:02 +0100

    push fine esercizio con bonus

---
+++ main.js
@@ -0,0 +1,21 @@
+const app = new Vue(
+    {
+        el: '#vue',
+        data: {
+            emailGenerata:'',
+            emailCasuali:[],
+        },
+
+        mounted(){
+            
+            for (let index = 0; index < 10; index++) {
+                    axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                    .then((email) => {
+                    this.emailGenerata = email.data.response
+                    this.emailCasuali.push(this.emailGenerata);
+                });   
+            }
+            console.log(this.emailCasuali);
+        }
+    }
+)
